60a4c3d7a64bed9f88b8910885b8c1671c3d84b4
Pavel <pv1222169@gmail.com>
2023-02-26 23:38:34 +0300

    Items collected and UI was append

--- Assets/Scripts/GameBehavior.cs
+++ Assets/Scripts/GameBehavior.cs
@@ -1,13 +1,57 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameBehavior : MonoBehaviour
 {
+    public string labelText = "Collect all 4 items and win, your freedom!";
+    public int maxItems = 4;
+    
     [SerializeField] private int _itemsCollected = 0;
     [SerializeField] private int _playerHP = 10;
-    
-    
+
+    public int Items
+    {
+        get { return _itemsCollected; }
+
+        set
+        {
+            _itemsCollected = value;
+            Debug.LogFormat("Items: {0}", _itemsCollected);
+
+            if (_itemsCollected >= maxItems)
+            {
+                labelText = "You`ve found all the items!";
+            }
+            else
+            {
+                labelText = "Item found, only " + (maxItems - _itemsCollected) + " more to go!";
+            }
+        }
+        
+}
+
+    public int HP
+    {
+        get { return _playerHP; }
+
+        set
+        {
+            _playerHP = value;
+            Debug.LogFormat("Lives: {0}", _playerHP);
+        }
+    }
+
+    private void OnGUI()
+    {
+        GUI.Box(new Rect(20, 20, 150, 25), "Player health: " + _playerHP);
+        
+        GUI.Box(new Rect(20, 50, 150, 25), "Items collected: " + _itemsCollected);
+        
+        GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height - 50, 300, 50), labelText);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/ItemBehaviour.cs
+++ Assets/Scripts/ItemBehaviour.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 
 public class ItemBehaviour : MonoBehaviour
 {
+    public GameBehavior gameManager;
+
+    private void Start() => gameManager = GameObject.Find("GameManager").GetComponent<GameBehavior>();
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.name == "Player")
         {
             Destroy(this.transform.parent.gameObject);
-            
             Debug.Log("Item collected");
+
+            gameManager.Items += 1;
         }
     }
 }
